958593006a8d820eda4012b562d7f7fd9192fd7b
Shubhodeep <banerjeeshubho98@gmail.com>
2024-01-30 14:48:45 +0530

    feature updates

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,33 @@
-import './CSS/style.css'
+import "./CSS/style.css";
+import FullList from "./model/FullList";
+import ListItem from "./model/ListItem";
+import ListTemplate from "./templates/ListTemplate";
 
+const initApp = (): void => {
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
+
+  itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault();
+
+// 
+
+  });
+
+  const clearItems = document.getElementById(
+    "clearItemsButton"
+  ) as HTMLButtonElement;
+
+  clearItems.addEventListener("click", (): void => {
+    fullList.clearList();
+    template.clear();
+  });
+
+  fullList.load();
+  template.render(fullList);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
